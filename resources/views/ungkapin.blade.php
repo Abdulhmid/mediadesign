3f9842168a016608c989a5e8f357f00a19a5f6e7
abdul <abdul.hamid@tlab.co.id>
2016-02-03 09:11:58 +0700

    first commit

---
+++ resources/views/ungkapin.blade.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="http://startbootstrap.com/assets/css/startbootstrap.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="{!! url('css/additional.css') !!}">
+
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">ungkap.in</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-nav-center">
+                        <li>
+                            <p class="navbar-btn">
+                                <a href="#" class="btn btn-default btn-top">Terpopuler</a>
+                            </p>
+                        </li>
+                        <li>
+                            <!-- <a href="/help" title="Help with Start Bootstrap Templates &amp; Themes">Naik Daun</a> -->
+                            <p class="navbar-btn">
+                                <a href="#" class="btn btn-default btn-top">Naik Daun</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p class="navbar-btn">
+                                <a href="#" class="btn btn-default btn-top">Terbaru</a>
+                            </p>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Premium Bootstrap Themes &amp; Templates"><i class="fa fa-star text-yellow"></i> Cari Hastag <b class="caret"></b></a>
+                        </li>
+                        <li>
+                            <!-- <a href="/contact" class="" title="Contact the Start Bootstrap Team">Ayo Ungkapkan</a> -->
+                            <p class="navbar-btn">
+                                <a href="#" class="btn btn-default">Ayo Ungkapin</a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+        <div class="clearfix"></div>
+        <section>
+            <div class="row col-condensed row-eq-height">
+                <div class="col-md-2 border">
+                    ASSASAMengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,    
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitatio
+                </div>
+                <div class="col-md-2 border">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitatio
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+                <div class="col-md-2 border">
+                    Mengapa Aku Begini, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation 
+                </div>
+            </div>            
+        </section>
+        <div class="clearfix"></div><hr/>
+        <section>
+            <footer>
+                <p><a href="http://www.startbootstrap.com">Start Bootstrap</a> is a project maintained by <a href="http://davidmiller.io">David Miller</a> at <a href="http://blackrockdigital.io">Blackrock Digital</a>.</p>
+            </footer>
+        </section>
+
+    </body>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+
+    </script>
+
+</html>
